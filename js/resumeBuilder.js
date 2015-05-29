b5fd6115bdb0e5dcceec23f2928a6286edefb69d
trikitrok <trikitrok@gmail.com>
2015-05-29 14:51:03 +0200

    Adds contacts list

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,23 +1,77 @@
-function formatElement(template, placeHolder, value) {
-  return template.replace(placeHolder, value);
+"use strict";
+
+function formatElements(template, substitutions) {
+  var i,
+    substitution,
+    res = template;
+
+  for (i = 0; i < substitutions.length; i++) {
+    substitution = substitutions[i];
+    res = res.replace(substitution.placeHolder, substitution.value);
+  }
+
+  return res;
 }
 
-function appendFormattedElement(nodeSelector, template, placeHolder, value) {
+function appendFormattedElement(nodeSelector, template, substitutions) {
   $(nodeSelector).append(
-    formatElement(template, placeHolder, value)
+    formatElements(template, substitutions)
   );
 }
 
-function preppendFormattedElement(nodeSelector, template, placeHolder, value) {
-  $(nodeSelector).preppend(
-    formatElement(template, placeHolder, value)
+function prependFormattedElement(nodeSelector, template, substitutions) {
+  $(nodeSelector).prepend(
+    formatElements(template, substitutions)
   );
 }
 
-appendFormattedElement(
-  "#header", HTMLheaderName, "%data%", "Manuel Rivero"
+var bio = {
+  name: "Manuel Rivero",
+  role: "Web Developer",
+  contacts: {
+    "mobile": "619492805",
+    "email": "trikitrok@gmail.com",
+    "twitter": "@trikitrok",
+    "github": "https://github.com/trikitrok",
+    "blog": "http://garajeando.blogspot.com.es/",
+    "location": "Barcelona",
+  },
+  picture: "../images/fry.jpg",
+  skills: [
+    "awesomeness",
+    "delivering things",
+    "caring"
+  ],
+  message: "Si la vida no me sonrie, yo le hago cosquillitas"
+};
+
+prependFormattedElement(
+  "#header", HTMLheaderRole, [{
+    placeHolder: "%data%",
+    value: bio.role
+  }]
 );
 
-appendFormattedElement(
-  "#header", HTMLheaderRole, "%data%", "Web Developer"
+prependFormattedElement(
+  "#header", HTMLheaderName, [{
+    placeHolder: "%data%",
+    value: bio.name
+  }]
 );
+
+function addContacts() {
+  for (var contactType in bio.contacts) {
+    appendFormattedElement(
+      "#topContacts",
+      HTMLcontactGeneric, [{
+        placeHolder: "%contact%",
+        value: contactType
+      }, {
+        placeHolder: "%data%",
+        value: bio.contacts[contactType]
+      }]
+    );
+  }
+}
+
+addContacts();
